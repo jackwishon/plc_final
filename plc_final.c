4798247167a4fd154230c6f8aa92bd6a1b0368b8
jackwishon <64821164+jackwishon@users.noreply.github.com>
2022-07-30 13:19:10 -0400

    initial commit

---
+++ plc_final.c
@@ -0,0 +1,73 @@
+//headers
+# include<stdio.h>
+# include<stdlib.h>
+# include<string.h>
+# include<ctype.h>
+
+//check keyword function
+int keywordMatch(char buffer[]) {
+
+    //keyword array
+    char keywords[32][10] = 
+    {"auto","break","case","char","const","continue","default",
+       "do","double","else","enum","extern","float","for","goto",
+       "if","int","long","register","return","short","signed",
+       "sizeof","static","struct","switch","typedef","union",
+       "unsigned","void","volatile","while"};
+
+    int i, flag = 0;
+
+    //iterate through keywords and raise flag if no match
+    for(i = 0; i < 32; ++i) {
+        if(strcmp(keywords[i], buffer) == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    return flag;
+}
+
+int main() {
+    //initialize operator array
+    char ch, buffer[15], operators[] = "+-*/%=";
+
+    FILE *fp;
+
+    int i, j = 0;
+
+    //update filename to whatever needs to be read
+    fp = fopen("readfile.txt", "r");
+
+    //error handling for opening file
+    if(fp == NULL) {
+        printf("error opening file");
+        exit(0);
+    }
+
+    //if operator return value
+    while((ch = fgetc(fp)) != EOF) {
+        for(i = 0; i < 6; ++i) {
+            if(ch == operators[i])
+                print("%c is operator\n", ch);
+        }
+
+        if(isalnum(ch)) {
+            buffer[j++] = ch;
+        }
+
+        else if((ch == ' ' || ch == '\n') && (j != 0)) {
+            buffer[j] = '\0';
+            j = 0;
+            if(isKeyword(buffer) == 1)
+                printf("%s is keyword\n", buffer);
+            else    
+                printf("%s is identifier\n", buffer);
+        }
+    }
+    
+    fclose(fp);
+
+    return 0;
+}
+
